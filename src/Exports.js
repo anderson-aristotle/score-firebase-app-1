d4cb12c386dd53d8def8d83660575ffcc2ca64af
rex@billion <anderson.aristotle@gmail.com>
2020-06-16 14:39:11 -0400

    added components and navigation

---
+++ src/Exports.js
@@ -0,0 +1,19 @@
+import Navigation from "./Navigation";
+import LandingPage from "./components/Landing";
+import SignUpPage from "./components/SignUp";
+import SignInPage from "./components/SignIn";
+import HomePage from "./components/Home";
+import PasswordForgetPage from "./components/PasswordForget";
+import AccountPage from "./components/Account";
+import AdminPage from "./components/Admin";
+
+export default {
+  Navigation,
+  LandingPage,
+  SignUpPage,
+  SignInPage,
+  PasswordForgetPage,
+  HomePage,
+  AccountPage,
+  AdminPage,
+};
---
+++ src/config/Firebase.js
@@ -0,0 +1,32 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+// Firebase API Config function
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+};
+
+// Initialize Firebase
+class Firebase {
+  constructor() {
+    firebase.initializeApp(config);
+
+    this.auth = firebase.auth();
+  }
+
+  // Firebase auth API
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  // doSignOut = () => this.auth.signout;
+  // doPasswordReset = (email) => this.auth.sendPasswordResetEmail(email);
+  // doPasswordUpdate = (password) => this.auth.updatePassword(password);
+}
+
+export default Firebase;
