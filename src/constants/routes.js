b1461e5cc81d65844c80589af28e2088276c63da
Charlie Kelly <charliekelly31415@gmail.com>
2020-06-19 01:07:55 -0400

    add nav routes array

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -5,3 +5,11 @@ export const HOME = "/home";
 export const ACCOUNT = "/account";
 export const ADMIN = "/admin";
 export const PASSWORD_FORGET = "/pw-forget";
+
+export const NAV_ROUTES = [
+  { route: SIGN_IN, text: 'Sign In' },
+  { route: LANDING, text: 'Landing' },
+  { route: HOME, text: 'Home' },
+  { route: ACCOUNT, text: 'Account' },
+  { route: ADMIN, text: 'Admin' },
+]
