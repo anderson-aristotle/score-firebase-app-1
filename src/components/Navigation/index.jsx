0b7768b07a9e660fd1a3c9632e953cfc32f681c3
Charlie Kelly <charliekelly31415@gmail.com>
2020-06-19 03:19:28 -0400

    handle auth navigation

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -2,14 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Nav from 'react-bootstrap/Nav';
 
+import SignOutButton from '../SignOut';
+import { AUTH_ROUTES, OPEN_ROUTES } from '../../constants/routes';
 
-const Navigation = ({ navLinks }) => (
-  <Nav className="nav-menu">
-  {navLinks.map((link, index) => (
-    <Nav.Item key={index}>
-      <Link to={link.route}>{link.text}</Link>
-    </Nav.Item>
-  ))}
-  </Nav>
-);
+
+const Navigation = ({ authUser }) => {
+  const links = authUser ? AUTH_ROUTES : OPEN_ROUTES;
+  return (
+    <Nav className="nav-menu">
+      {links.map((link, index) => (
+        <Nav.Item key={index}>
+          <Link to={link.route}>{link.text}</Link>
+        </Nav.Item>
+      ))}
+      {authUser && (
+        <Nav.Item>
+          <SignOutButton />
+        </Nav.Item>
+      )}
+    </Nav>
+  )
+};
+
+
 export default Navigation;
