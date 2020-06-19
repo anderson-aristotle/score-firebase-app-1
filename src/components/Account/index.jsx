8f8f2095dfd64540a819aede374f0c534075803e
Charlie Kelly <charliekelly31415@gmail.com>
2020-06-19 01:08:18 -0400

    expand Account page

--- src/components/Account/index.jsx
+++ src/components/Account/index.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
-const Account = () => (
-  <div>
-    <h1>Account</h1>
+import Container from 'react-bootstrap/Container';
+
+import { PasswordChangeForm } from '../PasswordChange';
+import { PasswordForgetForm } from '../PasswordForget';
+
+const AccountPage = () => (
+  <div className="auth-page">
+    <h1>Account Page</h1>
+    <Container fluid="sm">
+      <PasswordChangeForm />
+      <PasswordForgetForm />
+    </Container>
   </div>
 );
 
-export default Account;
+
+
+export default AccountPage;
