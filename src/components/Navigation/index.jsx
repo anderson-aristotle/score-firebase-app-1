e7e707a2df543047df26023558e17c286ace1c67
Charlie Kelly <charliekelly31415@gmail.com>
2020-06-19 01:08:33 -0400

    use routes array

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,29 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Nav from 'react-bootstrap/Nav';
 
-import Container from '@material-ui/core/Container';
 
-import * as ROUTES from "./constants/Routes";
-
-const Navigation = () => (
-  <Container>
-    <ul>
-      <li>
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.LANDING}>Landing</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.HOME}>Home</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ACCOUNT}>Account</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ADMIN}>Admin</Link>
-      </li>
-    </ul>
-  </Container>
+const Navigation = ({ navLinks }) => (
+  <Nav className="nav-menu">
+  {navLinks.map((link, index) => (
+    <Nav.Item key={index}>
+      <Link to={link.route}>{link.text}</Link>
+    </Nav.Item>
+  ))}
+  </Nav>
 );
 export default Navigation;
