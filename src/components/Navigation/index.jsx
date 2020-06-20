bd0c063cdca11dc98fbb394ab87002a31d39b480
Charlie Kelly <charliekelly31415@gmail.com>
2020-06-20 01:29:07 -0400

    move authContext to nav

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -6,30 +6,50 @@ import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 
 import { SignOutLink } from '../SignOut';
-import { AUTH_ROUTES, OPEN_ROUTES } from '../../constants/routes';
+import * as ROUTES from '../../constants/routes';
+import { AuthUserContext } from '../Session';
 
 import './nav.scss';
 
-const Navigation = ({ authUser }) => {
-  const links = authUser ? AUTH_ROUTES : OPEN_ROUTES;
-  return (
-    <Container fluid className="menu-wrapper">
-     <Nav className="menu justify-content-around">
-        {links.map((link, index) => (
-          <Nav.Item key={index}>
-            <Link className="nav-link" to={link.route}>{link.text}</Link>
-          </Nav.Item>
-        ))}
-        {authUser && (
-          <Nav.Item>
-            <SignOutLink />
-          </Nav.Item>
-        )}
-      </Nav>
-    </Container>
-    
-  )
-};
+const MenuAuth = () => (
+  <Nav className="menu auth-menu justify-content-around">
+    <Nav.Item>
+      <Link className="nav-link" to={ROUTES.HOME}>Home</Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Link className="nav-link" to={ROUTES.ACCOUNT}>Account</Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Link className="nav-link" to={ROUTES.ADMIN}>Admin</Link>
+    </Nav.Item>
+    <Nav.Item>
+      <SignOutLink />
+    </Nav.Item>
+  </Nav>
+);
 
+const MenuNoAuth = () => (
+  <Nav className="menu justify-content-around">
+    <Nav.Item>
+      <Link className="nav-link" to={ROUTES.SIGN_IN}>Sign In</Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Link className="nav-link" to={ROUTES.SIGN_UP}>Sign Up</Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Link className="nav-link" to={ROUTES.LANDING}>Landing</Link>
+    </Nav.Item>
+  </Nav>
+);
+
+const Navigation = () => (
+  <Container fluid="sm" className="menu-wrapper">
+    <AuthUserContext.Consumer>
+      {authUser => 
+        authUser ? <MenuAuth /> : <MenuNoAuth />
+      }
+    </AuthUserContext.Consumer>
+  </Container>
+);
 
 export default Navigation;
