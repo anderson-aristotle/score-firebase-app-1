3827160d42b797b2663c89ace336b73098b771cd
Charlie Kelly <charliekelly31415@gmail.com>
2020-06-19 21:25:48 -0400

    add accordion to Account page

--- src/components/Account/index.jsx
+++ src/components/Account/index.jsx
@@ -1,16 +1,58 @@
 import React from "react";
 
 import Container from 'react-bootstrap/Container';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
 
 import { PasswordChangeForm } from '../PasswordChange';
 import { PasswordForgetForm } from '../PasswordForget';
 
+import '../Common/page.scss';
+import '../Common/form.scss';
+
 const AccountPage = () => (
   <div className="auth-page">
-    <h1>Account Page</h1>
-    <Container fluid="sm">
-      <PasswordChangeForm />
-      <PasswordForgetForm />
+    <h1 className="page-title">Account Page</h1>
+    <Container fluid="sm" className="page-content">
+      <Accordion defaultActiveKey="0">
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="0">
+            Account Info
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>
+              <span className="todo">account info</span>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="1">
+            Change Password
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="1">
+            <Card.Body>
+              <div className="login-form">
+                <h3>Change Password</h3>
+                <PasswordChangeForm />
+              </div>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="2">
+            Reset Password
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="2">
+            <Card.Body>
+              <div className="login-form">
+                <h3 className="form-title">Reset Password</h3>
+                <PasswordForgetForm />
+              </div>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
+
     </Container>
   </div>
 );
