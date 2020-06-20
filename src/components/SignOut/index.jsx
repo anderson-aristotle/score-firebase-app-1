655cc4663c1168b3d850a0e2636ea355040a4362
Charlie Kelly <charliekelly31415@gmail.com>
2020-06-19 23:22:31 -0400

    add Link

--- src/components/SignOut/index.jsx
+++ src/components/SignOut/index.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-
 import { withFirebase } from '../Firebase';
 
 import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import { LANDING } from '../../constants/routes';
 
-const SignOutButton = ({ firebase }) => (
-  <Button onClick={firebase.doSignOut}>Sign Out</Button>
+
+export const SignOutLink = withFirebase(({ firebase }) => (
+  <Nav.Link href={LANDING} onClick={firebase.doSignOut}>Sign Out</Nav.Link>
+));
+
+const SignOutButton = ({ firebase, history, variant = 'light'}) => (
+  <Button variant={variant} onClick={() => {
+    firebase.doSignOut();
+    history.push(LANDING);
+  }}>Sign Out</Button>
 );
 
 export default withFirebase(SignOutButton);
