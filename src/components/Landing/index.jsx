cc97ef5939ba07071b562aa8558af9d3f8f2b504
Charlie Kelly <charliekelly31415@gmail.com>
2020-06-20 01:28:30 -0400

    add 404 page

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { useLocation } from 'react-router-dom';
 import Container from "react-bootstrap/Container";
+
+export const DefaultPage = () => {
+  let location = useLocation();
+
+  return (
+    <Container fluid className="page-content">
+      <header>
+        <h3 className="page-title">Page Not Found</h3>
+        <p>
+          The page are looking for, <span>{location.pathname}</span>,
+          could not be found.
+        </p>
+      </header>
+      
+    </Container>
+  )
+}
 
 
 const LandingPage = () => (
