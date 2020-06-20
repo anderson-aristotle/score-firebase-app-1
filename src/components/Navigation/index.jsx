8419b92baca79a772e24b9d08f14ee698af02c18
Charlie Kelly <charliekelly31415@gmail.com>
2020-06-19 21:30:23 -0400

    improve menu appearance

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,26 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+// import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 
 import SignOutButton from '../SignOut';
 import { AUTH_ROUTES, OPEN_ROUTES } from '../../constants/routes';
 
+import './nav.scss';
 
 const Navigation = ({ authUser }) => {
   const links = authUser ? AUTH_ROUTES : OPEN_ROUTES;
   return (
-    <Nav className="nav-menu">
-      {links.map((link, index) => (
-        <Nav.Item key={index}>
-          <Link to={link.route}>{link.text}</Link>
-        </Nav.Item>
-      ))}
-      {authUser && (
-        <Nav.Item>
-          <SignOutButton />
-        </Nav.Item>
-      )}
-    </Nav>
+    <Container fluid className="menu-wrapper">
+     <Nav className="menu justify-content-around">
+        {links.map((link, index) => (
+          <Nav.Item key={index} style={{padding: '0 1em'}}>
+            <Link to={link.route}>{link.text}</Link>
+          </Nav.Item>
+        ))}
+        {authUser && (
+          <Nav.Item>
+            <SignOutButton />
+          </Nav.Item>
+        )}
+      </Nav>
+    </Container>
+    
   )
 };
 
