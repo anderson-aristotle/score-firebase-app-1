e4a866d3f47b4a38d284a49dfdf448df34c30989
Charlie Kelly <charliekelly31415@gmail.com>
2020-06-19 23:25:39 -0400

    temp placeholder for .env

---
+++ src/config/fb.js
@@ -0,0 +1,11 @@
+// ONLY HERE BECAUSE process.env not loading
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
