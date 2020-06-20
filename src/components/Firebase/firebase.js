7829a0447083555a69ef72c02a4cd88ac74b2f23
Charlie Kelly <charliekelly31415@gmail.com>
2020-06-19 23:23:03 -0400

    config workaround

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,23 +2,28 @@ import app from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
+import { firebaseConfig } from '../../config/fb';
+
 // Firebase API Config function
-const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_ID,
-  measurementId: process.env.REACT_MEASUREMENT_ID
-};
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FB_API_KEY,
+//   authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_FB_DATABASE_URL,
+//   projectId: process.env.REACT_APP_FB_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_G_APP_ID,
+//   measurementId: process.env.REACT_APP_G_MEASUREMENT_ID
+// };
+
+
 
 // Initialize Firebase
 class Firebase {
   constructor() {
-    app.initializeApp(config);
-    // app.analytics();
+    app.initializeApp(firebaseConfig);
+    
+    this.auth = app.auth();
   }
 
   // Auth API
