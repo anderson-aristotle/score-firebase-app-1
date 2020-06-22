6843a43a7d03a0e52c8d464c0a23126ab2488ecd
Charlie Kelly <charliekelly31415@gmail.com>
2020-06-21 23:27:53 -0400

    keys for RexBillions cnx

--- src/config/fb.js
+++ src/config/fb.js
@@ -1,11 +1,11 @@
 // ONLY HERE BECAUSE process.env not loading
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyB8lN2-NlNEWSr3FiYVGDy95XitPHGvJPE",
+  authDomain: "rexbillions-efef6.firebaseapp.com",
+  databaseURL: "https://rexbillions-efef6.firebaseio.com",
+  projectId: "rexbillions-efef6",
+  storageBucket: "rexbillions-efef6.appspot.com",
+  messagingSenderId: "251391136301",
+  appId: "1:251391136301:web:5ef0ea1d772e6b398f5ea4",
+  measurementId: "G-YG2EE63PS5"
 };
