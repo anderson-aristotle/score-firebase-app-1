5cce3261b5adad3978dc810ab8e30c5b42aa3907
Charlie Kelly <charliekelly31415@gmail.com>
2020-06-23 12:52:57 -0400

    config switcher

--- src/config/fb.js
+++ src/config/fb.js
@@ -1,11 +1,27 @@
 // ONLY HERE BECAUSE process.env not loading
+
+// REX-BILLIONS instance
 export const firebaseConfig = {
-  apiKey: "AIzaSyB8lN2-NlNEWSr3FiYVGDy95XitPHGvJPE",
-  authDomain: "rexbillions-efef6.firebaseapp.com",
-  databaseURL: "https://rexbillions-efef6.firebaseio.com",
-  projectId: "rexbillions-efef6",
-  storageBucket: "rexbillions-efef6.appspot.com",
-  messagingSenderId: "251391136301",
-  appId: "1:251391136301:web:5ef0ea1d772e6b398f5ea4",
-  measurementId: "G-YG2EE63PS5"
-};
+  development: {
+    apiKey: "AIzaSyB8lN2-NlNEWSr3FiYVGDy95XitPHGvJPE",
+    authDomain: "rexbillions-efef6.firebaseapp.com",
+    databaseURL: "https://rexbillions-efef6.firebaseio.com",
+    projectId: "rexbillions-efef6",
+    storageBucket: "rexbillions-efef6.appspot.com",
+    messagingSenderId: "251391136301",
+    appId: "1:251391136301:web:5ef0ea1d772e6b398f5ea4",
+    measurementId: "G-YG2EE63PS5"
+  },
+  production: {
+    apiKey: "AIzaSyDdwRVQdT-fgsELG0QqA7oAgyzswZWck34",
+    authDomain: "score-app-93a39.firebaseapp.com",
+    databaseURL: "https://score-app-93a39.firebaseio.com",
+    projectId: "score-app-93a39",
+    storageBucket: "score-app-93a39.appspot.com",
+    messagingSenderId: "623919928687",
+    appId: "1:623919928687:web:0e53af8962de6d6fb4849a",
+    measurementId: "G-V6SV2W45SW"
+  }
+}
+
+
